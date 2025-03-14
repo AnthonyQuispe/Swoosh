c3cd1d8faa5ebfe26983f6db7e40474427cc37ae
Anthony <anthony.s.quispe@gmail.com>
2025-03-14 18:29:59 -0400

    Update Google Button back end functionailty and attached to firebase firestore to store user data

--- src/components/GoogleButton/GoogleButton.jsx
+++ src/components/GoogleButton/GoogleButton.jsx
@@ -1,9 +1,45 @@
 import "./GoogleButton.scss";
 import GoogleIcon from "../../assets/icons/GoogleIcon.png";
 
+import { doc, setDoc } from "firebase/firestore";
+import { auth, db } from "../../firebase/FirebaseConfig";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+
 export default function GoogleButton() {
+  const provider = new GoogleAuthProvider();
+  const navigate = useNavigate();
+
+  const handleGoogleButton = () => {
+    signInWithPopup(auth, provider)
+      .then(async (result) => {
+        const user = result.user;
+        const userRef = doc(db, "users", user.uid);
+
+        const userData = {
+          email: user.email,
+        };
+
+        await setDoc(userRef, userData, { merge: true });
+
+        navigate("/dashboard", {
+          state: {
+            userEmail: auth.currentUser.email,
+            displayName: user.displayName,
+          },
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <button className="google-button">
+    <button
+      className="google-button"
+      onClick={handleGoogleButton}
+      type="button"
+    >
       <img alt="google Icon" src={GoogleIcon} className="google__img" />
       <p className="google__description">Sign in with Google</p>
     </button>
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,14 @@
+import "./Profile.scss";
+
+export default function Profile() {
+  return (
+    <>
+      <div>
+        <p>@CoolKid</p>
+        <div>
+          <img />
+        </div>
+      </div>
+    </>
+  );
+}
